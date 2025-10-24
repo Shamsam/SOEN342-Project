1f4609675b659caa34c0aa351ff9a7a4a1d9a0f5
Noah <noah.nemes-lauzier@drakkar.com>
2025-10-24 18:19:14 -0400

    fixed Ticket and created Booking class

--- demo/src/main/java/railsystem/Ticket.java
+++ demo/src/main/java/railsystem/Ticket.java
@@ -1,16 +1,37 @@
 package railsystem;
-import java.util.List;
 
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class Ticket {
     Trip trip;
-    List<Traveller> travellers;
+    Traveller traveller;
+    String classRate;
 
-    public Ticket(Trip trip, List<Traveller> travellers) {
+
+    public Ticket(Trip trip, Traveller traveller, String classRate) {
         this.trip = trip;
-        this.travellers = travellers;
+        this.traveller = traveller;
+        this.classRate = classRate;
+    }
+
+    public BigDecimal getTotalCost() {
+        BigDecimal cost;
+
+        if(classRate.equals("First Class")){
+            cost = trip.getConnections().stream()
+                    .map(c -> c.getTicketRates().getFirstClass())
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+        } else if (classRate.equals("Second Class")){
+            cost = trip.getConnections().stream()
+                    .map(c -> c.getTicketRates().getSecondClass())
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+        }
+        else {
+            throw new IllegalArgumentException("Invalid class rate: " + classRate);
+        }
+        return cost;
     }
 
     @Override
@@ -19,10 +40,10 @@ public class Ticket {
         sb.append("════════════════════════ TICKET ════════════════════════\n");
         sb.append(trip.toString());
         sb.append("──────────────────────────────────────────────────────────\n");
-        sb.append("Travellers:\n");
-        for (Traveller traveller : travellers) {
-            sb.append("- ").append(traveller.getLastName()).append(", ").append(traveller.getLastName()).append("\n");
-        }
+        sb.append("Traveller:\n");
+        sb.append("- ").append(traveller.getLastName()).append(", ").append(traveller.getLastName()).append("\n");
+        sb.append("Class Rate: ").append(classRate).append("\n");
+        sb.append("Total Cost: $").append(getTotalCost()).append("\n");
         sb.append("══════════════════════════════════════════════════════════\n");
         return sb.toString();
 }
